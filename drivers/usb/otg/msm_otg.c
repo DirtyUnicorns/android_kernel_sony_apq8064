e901ac72d68c378a125cc28cdc8c51409242e13b
motley <motley.slate@gmail.com>
2012-12-23 09:37:36 -0500

    USB Force Fast charge
    Change-Id: I8c49434ce7ec0f80c627fb7ff1165165e1ac9341

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -60,8 +60,14 @@
 #include <mach/msm_bus.h>
 #include <mach/rpm-regulator.h>
 
+
 #ifdef CONFIG_USB_HOST_EXTRA_NOTIFICATION
 #include <linux/usb/host_ext_event.h>
+#endif
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* uA */
 #endif
 
 #define MSM_USB_BASE	(motg->regs)
@@ -1182,7 +1188,14 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 
 	if (motg->cur_power == mA)
 		return;
-
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge == 1) {
+			mA = USB_FASTCHG_LOAD;
+			pr_info("USB fast charging is ON - 1000mA.\n");
+	} else {
+		pr_info("USB fast charging is OFF.\n");
+	}
+#endif
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
 
 	/*
