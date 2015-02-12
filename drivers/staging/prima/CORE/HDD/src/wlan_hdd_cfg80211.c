7b392c49bb05985fa21ee9dc2c10553f20feb161
olivieer <yves.olli@gmail.com>
2015-02-12 19:28:06 +0100

    staging: prima: Sync with caf/LA.AF.1.1_rb1.12
    353a890 : Merge "wlan: Fix to avoid SAP operating in DFS channel."
    069287a : Merge "wlan: From HDD remove the logic to try shared auth if open fails."
    ba50efc : Merge "Wlan: Check for Roam command in csrIsRoamCommandWaitingForSession."
    b502483 : Merge "wlan: Fix to return proper value from wlan_hdd_scan_abort"
    4749f97 : wlan: Fix to avoid SAP operating in DFS channel.
    7c867e5 : wlan: From HDD remove the logic to try shared auth if open fails.
    1e12326 : Wlan: Check for Roam command in csrIsRoamCommandWaitingForSession.
    dc291ff : Merge "TDLS: Fix possible memory poisoning while processing tdls commands"
    bfac3ab : wlan: Fix to return proper value from wlan_hdd_scan_abort
    9c97a98 : TDLS: Fix possible memory poisoning while processing tdls commands
    87f5c75 : Merge "wlan: Send proper mlm cnf when Add BSS failed during roaming"
    3d920b6 : Merge "wlan: Initialize local variable len."
    598857a : wlan: Initialize local variable len.
    9e6ef64 : wlan: Send proper mlm cnf when Add BSS failed during roaming
    
    As per: https://www.codeaurora.org/cgit/quic/la/platform/vendor/qcom-opensource/wlan/prima/commit/?h=LA.AF.1.1_rb1.12&id=353a8909ac9c2aea1f70faec7d5b19dd936ff494
    
    Change-Id: I9b8a4f33b67eead07ad7a5389be6e62ea19eba6c

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -2344,7 +2344,7 @@ static int __wlan_hdd_cfg80211_stop_ap (struct wiphy *wiphy,
 
     ret = wlan_hdd_scan_abort(pAdapter);
 
-    if (ret <= 0)
+    if (ret < 0)
     {
         VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
                    FL("Timeout occurred while waiting for abortscan %ld"), ret);
@@ -7852,7 +7852,7 @@ static int wlan_hdd_cfg80211_sched_scan_start(struct wiphy *wiphy,
     }
 
     ret = wlan_hdd_scan_abort(pAdapter);
-    if (ret <= 0)
+    if (ret < 0)
     {
         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
                   "%s: aborting the existing scan is unsuccessfull", __func__);
@@ -7914,7 +7914,9 @@ static int wlan_hdd_cfg80211_sched_scan_start(struct wiphy *wiphy,
     num_ch = 0;
     if (request->n_channels)
     {
-        for (i = 0; i < request->n_channels; i++)
+        char chList [(request->n_channels*5)+1];
+        int len;
+        for (i = 0, len = 0; i < request->n_channels; i++)
         {
             for (indx = 0; indx < num_channels_allowed; indx++)
             {
@@ -7930,15 +7932,13 @@ static int wlan_hdd_cfg80211_sched_scan_start(struct wiphy *wiphy,
                         break;
                     }
                     valid_ch[num_ch++] = request->channels[i]->hw_value;
-
-                    VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
-                    "%s : pabx: Keeping DFS channel : %d",
-                    __func__,channels_allowed[indx]);
-
+                    len += snprintf(chList+len, 5, "%d ",
+                                     request->channels[i]->hw_value);
                     break ;
                 }
             }
         }
+        hddLog(VOS_TRACE_LEVEL_INFO,"Channel-List:  %s ", chList);
 
         /*If all channels are DFS and dropped, then ignore the PNO request*/
         if (num_ignore_dfs_ch == request->n_channels)
